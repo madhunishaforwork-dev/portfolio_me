4d0cfb3b7b2de35a5cf117e0ce2130d4ac7ec32c
madhunishaforwork-dev <madhu.nisha.for.work@gmail.com>
2026-02-22 18:53:34 +0530

    Fix deployment paths and update theme to Red

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/portfolio_madhu/',
+    base: '/portfolio_me/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
